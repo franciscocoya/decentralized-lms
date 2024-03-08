1a4ca4eee48ca6dda6f58986831046a4195dba22
franciscocoya <kikocoya@gmail.com>
2024-03-08 18:54:41 +0100

    fix: lint errors al global-error file

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -4,7 +4,9 @@ import * as Sentry from "@sentry/nextjs";
 import Error from "next/error";
 import { useEffect } from "react";
 
-export default function GlobalError({ error }) {
+export default function GlobalError({ error }:{
+  error: Error;
+}) {
   useEffect(() => {
     Sentry.captureException(error);
   }, [error]);
