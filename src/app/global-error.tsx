81195217376da51039b5acb2bdef6d09c35cc58c
franciscocoya <kikocoya@gmail.com>
2024-03-08 19:20:57 +0100

    fix: sonarlint error component status code

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -4,17 +4,15 @@ import * as Sentry from "@sentry/nextjs";
 import Error from "next/error";
 import { useEffect } from "react";
 
-export default function GlobalError({ error }:{
-  error: Error;
-}) {
+export default function GlobalError({ error }: { readonly error: Error }) {
   useEffect(() => {
     Sentry.captureException(error);
   }, [error]);
 
   return (
-    <html>
+    <html lang={"es"}>
       <body>
-        <Error />
+        <Error statusCode={404} />
       </body>
     </html>
   );
