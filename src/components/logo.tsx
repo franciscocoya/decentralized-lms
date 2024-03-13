c352b99b3f7163dfc39505f9bfca7945fecc2007
franciscocoya <kikocoya@gmail.com>
2024-03-13 23:48:12 +0100

    refactor: remove innecesary code from logo component

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,9 +3,6 @@ import React from "react";
 
 const Logo: React.FC = () => {
   return (
-    // <a href="/" className="font-bold text-2xl">
-    //   <span>DeLearn</span>
-    // </a>
     <Image src="/assets/images/logo_black_crop.png" alt="App logo" width={100} height={50} />
   );
 };
